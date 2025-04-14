3ffd5770d60c4c62bd0327bd606b91dda2802c0e
mmizin <mizmannikolay@gmail.com>
2025-04-14 20:40:16 +0100

    Start working

---
+++ calculator.ts
@@ -0,0 +1,8 @@
+type investmentData = {
+    initioalAmiunt: number;
+    annualContribution: number,
+    expectedReturn: number,
+    duration: number,
+}
+
+function calculateInvestment(data: investmentData) {}
